a91530541f474169e1ee4520ce13af1d350b76c3
joaquimnetocel <joaquim.neto.cel@gmail.com>
2025-04-29 11:58:18 -0300

    PRISMA INSTALADO

---
+++ src/lib/server/prismaClient.ts
@@ -0,0 +1,8 @@
+import { PrismaClient } from '@prisma/client';
+// OU: import { PrismaClient } from '@prisma/client/edge'
+
+const prisma = global.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV === 'development') {
+	global.prisma = prisma;
+}
